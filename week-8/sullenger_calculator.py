a3e558cb96f56156d020a22963a525d53d1a71aa
Jason Sullenger <jssullenger@my365.bellevue.edu>
2019-06-18 20:16:41 -0600

    adding week 8

---
+++ week-8/sullenger_calculator.py
@@ -0,0 +1,17 @@
+first_name = "Jason"
+last_name = "Sullenger"
+
+def add(first, second):
+    return first + second
+
+def subtract(first, second):
+    return first - second
+
+def divide(first, second):
+    return first / second
+
+print(first_name + " " + last_name)
+print(' ')
+print(add(3, 2))
+print(subtract(8, 2))
+print(divide(10, 4))
