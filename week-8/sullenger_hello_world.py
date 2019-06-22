f6c91721554251ae493865b04c752f390aaba2f1
Jason Sullenger <jssullenger@my365.bellevue.edu>
2019-06-21 20:34:55 -0600

    correcting attribution

--- week-8/sullenger_hello_world.py
+++ week-8/sullenger_hello_world.py
@@ -1,1 +1,9 @@
+# ============================================
+# ; Title:  sullenger_hello_world.py
+# ; Author: Professor Krasso
+# ; Date:  21 June 2019
+# ; Modified By: Jason Sullenger
+# ; Description: Hello World
+# ;===========================================
+
 print("Hello World")
